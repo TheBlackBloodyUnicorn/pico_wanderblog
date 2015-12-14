6a7614a2bc8a35fba12b650f4566b3a9096943bb
YoanM <yoanmontoya@yahoo.fr>
2015-12-14 15:55:02 +0000

    corrected a function

--- models/Model_user.php
+++ models/Model_user.php
@@ -4,9 +4,9 @@ class Model_user
 
 	public static function sign_in($username, $password){
 		global $rep, $vues, $TmessagesConnection;
+
+		$user = DAL::getUser(Cleaning::cleanString($username),Cleaning::cleanString($password)); //on teste si c'est dans la bdd (en nettoyant au passage)
 		
-		$user=DAL::getUser(Cleaning::cleanString($username),sha1(Cleaning::cleanString($password))); //on teste si c'est dans la bdd (en nettoyant au passage)
-	
 		if(!is_null($user)){
 			$_SESSION['username'] = $user->getUsername();
 			$_SESSION['role'] = $user->getRole();
