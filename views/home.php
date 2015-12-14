6a2734a6ecb88e398cf50e4846373098069abdb0
YoanM <yoanmontoya@yahoo.fr>
2015-12-14 15:55:32 +0000

    siple login form test

--- views/home.php
+++ views/home.php
@@ -1,11 +1,7 @@
-<h1>This is a test</h1>
 <form method="post" action="index.php">
-	<input type="hidden" value="home" name="action" id="action"/>
-	<input type="submit" name="" value="add">
-</form>
-<?php
-if(isset($user2display)){
-	echo $user2display->getRole();
-}
+        <p><input type="text" name="username" value="" placeholder="Username or Email"></p>
+        <p><input type="password" name="password" value="" placeholder="Password"></p>
+        <input type="hidden" name="sign_in">
+        <p class="submit"><input type="submit" name="action" value="sign_in"></p>
 
-?>
+</form>
