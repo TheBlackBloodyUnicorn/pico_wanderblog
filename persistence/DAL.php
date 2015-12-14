c98e794a6199b56f71371f90d7944cb89c5de3c4
YoanM <yoanmontoya@yahoo.fr>
2015-12-14 22:01:00 +0000

    added the possibility to add a user to the database

--- persistence/DAL.php
+++ persistence/DAL.php
@@ -1,17 +1,24 @@
-<?php 
+<?php
 
 class DAL{
 
-	static function  getUser($username,$password)
+	static function getUser($username,$password)
 	{
 		$req='SELECT * FROM registered_user WHERE username=? AND password=?';
 		$param  = array(0 => array($username, PDO::PARAM_STR) , 1 => array($password, PDO::PARAM_STR));
 		$res= DB::getInstance()->prepareAndExecuteQueryWithResult($req,$param);
 		$user=NULL;
 		foreach ($res as $data) {
-			$user=new User($data["username"],$data["password"],$data["user_level"]);
+			$user=new User($data["username"],$data["password"],$data["user_level"], $data["email"], $data["country"], $data["is_approved"]);
 		}
 		return $user;
+	}
+
+	static function addUser($username, $password, $email, $role, $isApproved, $country)
+	{
+		$req='INSERT INTO registered_user (username, password,email, user_level, is_approved, nationality) VALUES(?,?,?,?,?,?)';
+		$param  = array(0 => array($username, PDO::PARAM_STR) , 1 => array($password, PDO::PARAM_STR),2 => array($email, PDO::PARAM_STR),3 => array($role, PDO::PARAM_STR),4 => array($isApproved, PDO::PARAM_BOOL),5 => array($country, PDO::PARAM_STR));
+		DB::getInstance()->prepareAndExecuteQueryWithoutResult($req,$param);
 	}
 }
 ?>
