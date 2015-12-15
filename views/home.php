fab6de3eaaf7442aa61c87cac16f8120179c7efc
davidg95 <pirakaleader@googlemail.com>
2015-12-15 13:32:23 +0000

    added stuff to home pagevbnmbj

--- views/home.php
+++ views/home.php
@@ -8,7 +8,7 @@
 
 <html>
 <head>
-        <link rel = "stylesheet" href = "style.css">
+        <link rel = "stylesheet" href = "style.css" type="text/css">
         <title>Walk-a-blog | Home</title>
 </head>
 <body>
