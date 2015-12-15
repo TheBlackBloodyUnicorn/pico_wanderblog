4ddb63700f3dc5801ad1627683850993fa4c8a6c
YoanM <yoanmontoya@yahoo.fr>
2015-12-15 22:53:42 +0000

    display the title of each adventures

--- views/home.php
+++ views/home.php
@@ -1,3 +1,6 @@
+<?php
+    require_once('common_functions.php');
+?>
 <html>
 <head>
         <link rel = "stylesheet" href = "style.css" type="text/css">
@@ -37,6 +40,13 @@
         <h1>Welcome to Walk-a-blog!</h1>
         <p>This website is all about travelling. Share your experience with other travellers.</p>
         <div id="adventure-container">
+          <?php
+          if(isset($adventures)){
+            foreach($adventures as $adventure){
+              displayAdventureCompactForm($adventure);
+            }
+          }
+          ?>
         </div>
     </div>
 </div>
