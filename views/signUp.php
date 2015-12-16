57971f89e9affd031bda6c5a1ae6d584edcfa9c8
1305997 <andrewrore94@gmail.com>
2015-12-16 10:39:26 +0000

    This is a blank sign up page, just testing pusing to github

---
+++ views/signUp.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Walk-a-blog | Sign up.</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+<div id="container">
+    <form>
+        Username:<br>
+        <input type = "text" name = username>
+        <br>
+        Password:<br>
+        <input type ="text" name = password>
+        <br>
+        Confirm password:<br>
+        <input type = "text" name = confirmPass>
+        <br>
+        Role:<br>
+        <input type = "radio" name = role1 value = Reader>
+        <br>
+        <input type = "radio" name = role2 value = Author>
+        <br>
+        <input type = "radio" name = role3 value = Administrator>
+        <br>
+        Email:<br>
+        <input type = "text" name = email>
+        <br>
+        Country:<br>
+        <input type = "text" name = country>
+        <br>
+    </form>
+</div>
+</body>
+</html>
