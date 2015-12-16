2b315bae236001d21fe9aa8dd1a62d2800abb228
davidg95 <pirakaleader@googlemail.com>
2015-12-16 16:10:39 +0000

    background image

--- views/common_functions.php
+++ views/common_functions.php
@@ -1,7 +1,7 @@
 <?php
 function displayAdventureCompactForm($adventure){
   echo "<div id='adventure-post'>";
-  echo "<h1><a href=\"index.php?action=display_adventure&id_adventure=".$adventure->getId()."\">".$adventure->getTitle()."</a></h1>";
+  echo "<h1 id='adventure-post'><a href=\"index.php?action=display_adventure&id_adventure=".$adventure->getId()."\">".$adventure->getTitle()."</a></h1>";
   echo "<img src=\"".$adventure->getPhotos()[0]."\" height=\"250\" width=\"500\"/>";
   echo "<p>Created by : ".$adventure->getAuthor()."</p>";
   echo "<p>Votes : ".$adventure->getNumberOfVotes()."</p>";
