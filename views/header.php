453bc1231a9d19fa680237f430b6f87d815b3c59
davidg95 <pirakaleader@googlemail.com>
2015-12-16 14:17:19 +0000

    Header php file

--- views/header.php
+++ views/header.php
@@ -7,23 +7,21 @@ echo "<body>";
 echo "<div id=\"container\">";
     echo "<header>";
         echo "<div id=\"topbar\">";
-            echo "<?php";
-            echo "if(!isset(\$_SESSION['logged']) || !\$_SESSION['logged']){";
-                echo "echo \"<div id=\"sign\">\";";
-                echo "echo \"<form method=\"post\" action=\"index.php\">\";";
-                echo "echo \"<input type=\"text\" name=\"username\" placeholder=\"Username\">\";";
-                echo "echo \"<input type=\"password\" name=\"password\" placeholder=\"Password\">\";";
-                echo "echo \"<input type=\"hidden\" name=\"action\" value=\"sign_in\">\";";
-                echo "echo \"<input type=\"submit\" name=\"sign_in\" value=\"sign in\">\";";
-                echo "echo \"</form>\";";
-            echo "}else{";
-                echo "echo \"<form method=\"post\" action=\"index.php\">\";";
-                echo "echo \"<input type=\"hidden\" name=\"action\" value=\"sign_out\">\";";
-                echo "echo \"<input type=\"submit\" name=\"\" value=\"sign out\">\";";
-                echo "echo \"</form>\";";
-                echo "echo \"</div>\";";
-            echo "}";
-            echo "?>";
+            if(!isset($_SESSION['logged']) || !$_SESSION['logged']){
+                echo "<div id=\"sign\">";
+                echo "<form method=\"post\" action=\"index.php\">";
+                echo "<input type=\"text\" name=\"username\" placeholder=\"Username\">";
+                echo "<input type=\"password\" name=\"password\" placeholder=\"Password\">";
+                echo "<input type=\"hidden\" name=\"action\" value=\"sign_in\">";
+                echo "<input type=\"submit\" name=\"sign_in\" value=\"sign in\">";
+                echo "</form>";
+            }else{
+                echo "<form method=\"post\" action=\"index.php\">";
+                echo "<input type=\"hidden\" name=\"action\" value=\"sign_out\">";
+                echo "<input type=\"submit\" name=\"\" value=\"sign out\">";
+                echo "</form>";
+                echo "</div>";
+            }
 echo "</div>";
 echo "<img src = \"http://placehold.it/1200x300\">";
 echo "</header>"
