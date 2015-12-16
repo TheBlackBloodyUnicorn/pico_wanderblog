a057f08652e0ace3d451563c31bba913d045b6b1
eoin9408 <eoin9408@gmail.com>
2015-12-16 17:16:09 +0000

    Added calls to getter methods to test them.

--- views/adventure.php
+++ views/adventure.php
@@ -1,5 +1,24 @@
-<?php
-if(isset($adventure)){
-  echo $adventure->getTitle();
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Walk-a-blog | Adventures.</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+<body>
+	<div id="container">
+		<div>
+			<?php
+				if(isset($adventure)){
+					echo <h1>$adventure->getTitle()</h1>;
+					echo <h2>$adventure->getAuthor()</h2>;
+					echo <p>$adventure->getCountry()</p>;
+					echo <p>$adventure->getDescription()</p>;
+					echo <p>$adventure->getId()</p>;
+					echo <p>$adventure->getNumberOfVotes()</p>;
+					
+				}
+			?>
+		</div>
+	</div>
+</body>
