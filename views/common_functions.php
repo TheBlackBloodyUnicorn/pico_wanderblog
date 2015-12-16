5f50c4f433217c93497650d04238aadacc83f27a
YoanM <yoanmontoya@yahoo.fr>
2015-12-16 20:13:09 +0000

    corrected an error

--- views/common_functions.php
+++ views/common_functions.php
@@ -2,7 +2,7 @@
 function displayAdventureCompactForm($adventure){
   echo "<div id='adventure-post'>";
   echo "<h1 id='adventure-post'><a href=\"index.php?action=display_adventure&id_adventure=".$adventure->getId()."\">".$adventure->getTitle()."</a></h1>";
-  echo "<img src=\"".$adventure->getPhotos()[0]."\" height=250 width=500/>";
+  echo "<img src=\"".$adventure->getPhotos()[0]."\" height=\"250\" width=\"500\"/>";
   echo "<p>Created by : ".$adventure->getAuthor()."</p>";
   echo "<p>Votes : ".$adventure->getNumberOfVotes()."</p>";
   echo "</div>";
