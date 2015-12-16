37f1588bb3dd7bbd798c42d3ebce35b7cd76ab1a
davidg95 <pirakaleader@googlemail.com>
2015-12-16 16:00:12 +0000

    background image

--- views/footer.php
+++ views/footer.php
@@ -1,5 +1,5 @@
 <?php
-    echo "<div id=\"footer\">";
+    echo "<div id='footer'>";
     echo "<a href=\"https://commons.wikimedia.org/wiki/File:Peyto_Lake-Banff_NP-Canada.jpg#/media/File:Peyto_Lake-Banff_NP-Canada.jpg\">Peyto Lake-Banff NP-Canada</a>\" by Tobias Alt, <a href=\"//commons.wikimedia.org/wiki/User:Tobi_87\" title=\"User:Tobi 87\">Tobi 87</a> - <span class=\"int-own-work\" lang=\"en\">Own work</span>. Licensed under <a href=\"http://www.gnu.org/copyleft/fdl.html\" title=\"GNU Free Documentation License\">GFDL</a> via <a href=\"//commons.wikimedia.org/wiki/\">Wikimedia Commons</a>.";
     echo "<p id='copyright'>&copy; Pico 2015</p>";
     echo "</div>";
