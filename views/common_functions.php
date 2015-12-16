6701e6fa098d648e9462780d2b7724323a6a4b24
davidg95 <pirakaleader@googlemail.com>
2015-12-16 16:11:51 +0000

    background image

--- views/common_functions.php
+++ views/common_functions.php
@@ -5,6 +5,7 @@ function displayAdventureCompactForm($adventure){
   echo "<img src=\"".$adventure->getPhotos()[0]."\" height=\"250\" width=\"500\"/>";
   echo "<p>Created by : ".$adventure->getAuthor()."</p>";
   echo "<p>Votes : ".$adventure->getNumberOfVotes()."</p>";
+  echo "<hr>";
   echo "</div>";
 }
 
