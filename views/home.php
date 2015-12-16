080f610c1df18b4b642b97be1280a8b47a91ffb4
1305997 <andrewrore94@gmail.com>
2015-12-16 11:14:01 +0000

    button to sign up page

--- views/home.php
+++ views/home.php
@@ -36,12 +36,12 @@
         <ul>
             <li><a href="home.php">Home</a></li>
             <li><a href="adventures.php">Adventures</a></li>
-
         </ul>
     </nav>
     <div id="content">
         <h1>Welcome to Walk-a-blog!</h1>
         <p>>This website is all about travelling. Share your experience with other travellers.</p>
+        <a href="signUp.php" class="button">Sign up here</a>
         <div id="adventure-container">
           <?php
           if(isset($adventures)){
