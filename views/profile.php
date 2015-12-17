21240fc391b2f957ad34981cbde3c2a9411b8ac8
davidg95 <pirakaleader@googlemail.com>
2015-12-17 14:05:54 +0000

    background image

--- views/profile.php
+++ views/profile.php
@@ -20,17 +20,6 @@ require_once('common_functions.php');
         $user = new User(1, 'davidg95', 'cheese', null, 'davidg95@hotmail.co.uk', 'UK', true);
         displayProfile($user);
         ?>
-        <h1>Welcome to Walk-a-blog!</h1>
-        <p>This website is all about travelling. Share your experience with other travellers. Click the link below to sign up!</p>
-        <div id="adventure-container">
-            <?php
-            if(isset($adventures)){
-                foreach($adventures as $adventure){
-                    displayAdventureCompactForm($adventure);
-                }
-            }
-            ?>
-        </div>
     </div>
     <?php
     include 'footer.php';
