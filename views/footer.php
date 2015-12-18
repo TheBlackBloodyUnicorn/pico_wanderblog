3a92d4a50f434fbad7b95fa6a3a11e941d3c58e1
davidg95 <pirakaleader@googlemail.com>
2015-12-18 11:46:16 +0000

    css for footer

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,6 @@
 <?php
-    echo "<div id='footer'>";
+    echo "<footer>";
     echo "<div id='link' class='copyright'><a href=\"https://commons.wikimedia.org/wiki/File:Peyto_Lake-Banff_NP-Canada.jpg#/media/File:Peyto_Lake-Banff_NP-Canada.jpg\">Peyto Lake-Banff NP-Canada</a>\" by Tobias Alt, <a href=\"//commons.wikimedia.org/wiki/User:Tobi_87\" title=\"User:Tobi 87\">Tobi 87</a> - <span class=\"int-own-work\" lang=\"en\">Own work</span>. Licensed under <a href=\"http://www.gnu.org/copyleft/fdl.html\" title=\"GNU Free Documentation License\">GFDL</a> via <a href=\"//commons.wikimedia.org/wiki/\">Wikimedia Commons</a>.</div>";
     echo "<p id='footer' class='copyright'>&copy; Pico 2015</p>";
-    echo "</div>";
+    echo "</footer>";
 ?>
