5cc181ac70f05a7b4c11f268ef4fa6c954700b85
eoin9408 <eoin9408@gmail.com>
2015-12-18 03:00:14 +0000

    Tweaked all_adventures.php

--- views/all_adventures.php
+++ views/all_adventures.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
+<?php
+    require_once('common_functions.php');
+	require('models/Adventure.php');
+	include 'header.php';
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Walk-a-blog | All Adventures</title>
     <link rel="stylesheet" href="./views/style.css" type="text/css"/>
 </head>
-<?php
-    require_once('common_functions.php');
-	require('models/Adventure.php');
-	include 'header.php';
-?>
 <body>
 	<div id="container">
 		<p>Test!</p>
 		<?php
 			echo "<p>Test!</p>";
 		
-			$adventures = array();
+			/*$adventures = array();
 			$adventures[0] = (new Adventure(1, "Adventure 01", "Test adventure", "Scotland", 01));
 			$adventures[1] = (new Adventure(2, "Adventure 02", "Another test adventure", "France", 02));
 			$adventures[2] = (new Adventure(3, "Adventure 03", "Yet another test adventure", "Austria", 01));
@@ -25,7 +25,7 @@
 				for($i = 0; $i < sizeof($adventures); $i++){
 					displayAdventureCompactForm($adventures[$i]);
 				}
-			}
+			}*/
 		?>
 	</div>
 </body>
