9ce113439dd3e5bf5ba945d020de88f9be52280a
davidg95 <pirakaleader@googlemail.com>
2015-12-18 12:44:36 +0000

    css for footer

--- views/header.php
+++ views/header.php
@@ -16,7 +16,7 @@
             echo "</form>";
             echo "</div>";
         }else{
-            echo $_SESSION->getUsername();
+            echo "<p> " . $_SESSION->getUsername() . "</p>";
             echo "<div id=\"sign\">";
             echo "<form method=\"post\" action=\"./index.php\">";
             echo "<input type=\"hidden\" name=\"action\" value=\"sign_out\">";
