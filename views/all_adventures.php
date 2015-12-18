f0f077b760854eddcb148749229aef94b8442fbb
eoin9408 <eoin9408@gmail.com>
2015-12-18 03:03:09 +0000

    tweaked all_adventures.php

--- views/all_adventures.php
+++ views/all_adventures.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php
     require_once('common_functions.php');
-	require('models/Adventure.php');
+	/*require_once('models/Adventure.php');*/
 	include 'header.php';
 ?>
 <html lang="en">
@@ -16,12 +16,12 @@
 		<?php
 			echo "<p>Test!</p>";
 		
-			/*$adventures = array();
+			$adventures = array();
 			$adventures[0] = (new Adventure(1, "Adventure 01", "Test adventure", "Scotland", 01));
 			$adventures[1] = (new Adventure(2, "Adventure 02", "Another test adventure", "France", 02));
 			$adventures[2] = (new Adventure(3, "Adventure 03", "Yet another test adventure", "Austria", 01));
 		
-			if(isset($adventures)){
+			/*if(isset($adventures)){
 				for($i = 0; $i < sizeof($adventures); $i++){
 					displayAdventureCompactForm($adventures[$i]);
 				}
