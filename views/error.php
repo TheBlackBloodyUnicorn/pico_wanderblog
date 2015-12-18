6371de9132903a5e57403b7d512e576002e1c7e1
davidg95 <pirakaleader@googlemail.com>
2015-12-18 12:37:24 +0000

    css for footer

--- views/error.php
+++ views/error.php
@@ -3,11 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <title>Walk-a-blog | Error.</title>
-    <link rel="stylesheet" href="style.css" type="text/css" />
+    <link rel = "stylesheet" href = "./views/style.css" type="text/css">
+    <link rel = "stylesheet" href = "style.css" type="text/css">
 </head>
 <body>
 <div id="container">
+    <?php
+    include 'header.php';
+    ?>
+    <div id = "content">
     <img src="./views/images/cat.jpg">
+    </div>
 </div>
 </body>
 </html>
